fafe39d87d301c3b2fc9fba51c7d7c977c75c385
Tan Jie <tanjie@paxsz.com>
2019-12-14 18:59:26 +0800

    update version

--- PaxstoreOpenApi/Properties/AssemblyInfo.cs
+++ PaxstoreOpenApi/Properties/AssemblyInfo.cs
@@ -6,5 +6,5 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-[assembly: AssemblyVersion("6.3.3")]
-[assembly: AssemblyFileVersion("6.3.3")]
+[assembly: AssemblyVersion("7.0.0")]
+[assembly: AssemblyFileVersion("7.0.0")]
--- PaxstoreOpenApiTest/TestConst.cs
+++ PaxstoreOpenApiTest/TestConst.cs
@@ -8,11 +8,17 @@ namespace Paxstore.Test
 {
     class TestConst
     {
-        public const string API_BASE_URL = "https://api.whatspos.com/p-market-api/";
-        public const string API_KEY = "MCXF60RUVJPKWGBGATYOX";
-        public const string API_SECRET = "JVVN1TCL9L2KXDNEGIE9OCTFXSGCN5VLS9ZN10RYX";
+        //public const string API_BASE_URL = "https://api.whatspos.com/p-market-api/";
+        //public const string API_KEY = "MCXF60RUVJPKWGBGATYOX";
+        //public const string API_SECRET = "JVVN1TCL9L2KXDNEGIE9OCTFXSGCN5VLS9ZN10RYX";
 
+        //public const string API_BASE_URL = "http://localhost:8080/p-market-api/";
+        //public const string API_KEY = "OSAYMR4YDFZJ4H8J6109";
+        //public const string API_SECRET = "FTZ0PS8VWNLEI4YET4DZWSKZ4KBM2QJBLXVJ4MGL";
 
+        public const string API_BASE_URL = "https://api.paxsit.com/p-market-api/";
+        public const string API_KEY = "0TAJEJK00R8YYK3HY0LI";
+        public const string API_SECRET = "R6JVSK1OI1IU6P64MXX9IRFMAIUCT6BG9OYPVNU2";
 
 
     }
