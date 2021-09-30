d56689a996a7f1ccf4f8eeeb06733e3407210cb9
tanjie <tanjie@paxsz.com>
2021-09-30 13:31:54 +0800

    update for version 8.2

--- PaxstoreOpenApi/Paxstore/OpenApi/Validator/Terminal/TerminalUpdateValidator.cs
+++ PaxstoreOpenApi/Paxstore/OpenApi/Validator/Terminal/TerminalUpdateValidator.cs
@@ -12,7 +12,8 @@ namespace Paxstore.OpenApi.Validator.Terminal
             RuleFor(x => x.MerchantName).MaximumLength(64);
             RuleFor(x => x.ResellerName).NotEmpty().MaximumLength(64);
             RuleFor(x => x.ModelName).NotEmpty().MaximumLength(64);
-            RuleFor(x => x.Location).MaximumLength(32);
+            RuleFor(x => x.Location).MaximumLength(64);
+            RuleFor(x => x.Remark).MaximumLength(64);
         }
     }
 }
