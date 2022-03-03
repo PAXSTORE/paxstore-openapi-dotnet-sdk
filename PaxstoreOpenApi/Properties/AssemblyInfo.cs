b6dc624550d9de91f8aa45d7a77b0fe3adec09ee
tanjie <tanjie@paxsz.com>
2022-03-03 15:31:19 +0800

    fix issue

--- PaxstoreOpenApi/Properties/AssemblyInfo.cs
+++ PaxstoreOpenApi/Properties/AssemblyInfo.cs
@@ -6,5 +6,5 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-[assembly: AssemblyVersion("8.2.0")]
-[assembly: AssemblyFileVersion("8.2.0")]
+[assembly: AssemblyVersion("8.2.1")]
+[assembly: AssemblyFileVersion("8.2.1")]
