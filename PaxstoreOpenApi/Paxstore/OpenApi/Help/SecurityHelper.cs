af9bffe328d1feb2ba1989b838be675e4876e0c2
tanjie <tanjie@paxsz.com>
2022-11-29 13:50:27 +0800

    update SecurityHelper

--- PaxstoreOpenApi/Paxstore/OpenApi/Help/SecurityHelper.cs
+++ PaxstoreOpenApi/Paxstore/OpenApi/Help/SecurityHelper.cs
@@ -22,5 +22,59 @@ namespace Paxstore.OpenApi.Help
             }
         }
 
+        public static byte[] GetMD5(string str)
+        {
+
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] result = md5.ComputeHash(System.Text.Encoding.Default.GetBytes(str));
+            byte[] result2 = new byte[result.Length];
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (result[i] > 128)
+                {
+                    int a = result[i] - 256;
+                    result2[i] = (byte)(result[i] - 256);
+                }
+                else
+                {
+                    result2[i] = result[i];
+                }
+            }
+            return result2;
+        }
+
+        public static string ByteToHexStr(byte[] bytes)
+        {
+            string result = "";
+            if (bytes != null)
+            {
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    result += bytes[i].ToString("X2");
+                }
+            }
+            return result;
+        }
+
+        public static string EncryptPasswordParameter(string content, string appSecret)
+        {
+            if (string.IsNullOrEmpty(content))
+            {
+                return null;
+            }
+            byte[] md5OfSecret = GetMD5(appSecret);
+            byte[] toEncryptArray = Encoding.UTF8.GetBytes(content);
+            RijndaelManaged rm = new RijndaelManaged
+            {
+                Key = md5OfSecret,
+                Mode = CipherMode.ECB,
+                Padding = PaddingMode.PKCS7
+            };
+
+            ICryptoTransform cTransform = rm.CreateEncryptor();
+            byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+            return ByteToHexStr(resultArray);
+        }
+
     }
 }
